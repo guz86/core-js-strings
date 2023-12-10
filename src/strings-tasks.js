2bd87489ec0948754de1902b09788f16d1d0ec47
Pavel Gordienko <guzmo6@rambler.ru>
2023-12-10 11:56:38 +0300

    feat: add solution removeFirstOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,14 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  const firstString = str.substring(0, index);
+  let lastString = str;
+  if (index !== -1) {
+    lastString = str.substring(index + value.length, str.length);
+  }
+  return firstString.concat(lastString);
 }
 
 /**
